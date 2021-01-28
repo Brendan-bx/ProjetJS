9265acbc9e4f4d0fb44e991a022ef5821181b651
Christopher Billy <billy_christopher@hotmail.com>
2021-01-28 15:15:33 +0100

    Ajout fonction qui charge les cours disponible via le JSON

--- PROJETJSAvancé/ProjetDepart/data/courses.js
+++ PROJETJSAvancé/ProjetDepart/data/courses.js
@@ -5,3 +5,37 @@ const COURSES = {
   4: {id: 4, img: 'node_js.jpg', title: 'Node JS', initial_price: 200, price: 9.99, mark: 5, stock: 3},
   5: {id: 5, img: 'my_sql.png', title: 'MySQL', initial_price: 200, price: 9.99, mark: 4, stock: 2}
 }
+
+createContainerItem();
+
+function createContainerItem() {
+  const container = document.querySelectorAll('.courses__container')[1];
+  const courses = Object.values(COURSES);
+  console.log(courses);
+
+  courses.forEach(course => {
+    let divItem = document.createElement('div');
+    divItem.classList.add('course__item');
+    divItem.innerHTML += `
+
+    <figure class="course_img">
+        <img src="img/courses/${course.img}">
+    </figure>
+    <div class="info__card">
+      <h4>${course.title}</h4>
+      <figure class="mark m_4">
+        <img src="img/rates.png">
+      </figure>
+      <p>
+        <span class="price">${course.initial_price} €</span>
+        <span class="discount">${course.price} €</span>
+      </p>
+      <p>
+        Disponible: <span class="stock">${course.stock}</span>
+      </p>
+      <a href="#" class="add-to-cart" data-id="1"><i class="fa fa-cart-plus"></i>Ajouter au panier</a>
+    </div>`
+
+    container.appendChild(divItem);
+  });
+}
--- PROJETJSAvancé/ProjetDepart/js/app.js
+++ PROJETJSAvancé/ProjetDepart/js/app.js
@@ -146,34 +146,6 @@ function deleteItemStorage(target) {
     localStorage.setItem('panierStorage', JSON.stringify(panierStorage));
 }
 
-
-// fonction de recherche
-SearchInput.addEventListener('keyup', recherche);
-
-function recherche() {
-    let filter, allTitles, titleValue, rien;
-    filter = SearchInput.value.toUpperCase();
-    allTitles = document.querySelectorAll('h4');
-    rien = document.querySelector('.hidden');
-    result = 0;
-
-    for (i = 0; i < allItems.length; i++) {
-
-        titleValue = allTitles[i].innerText;
-
-        if (titleValue.toUpperCase().indexOf(filter) > -1) {
-            allItems[i].style.display = "flex";
-            result++;
-            rien.style.display = "none";
-        } else {
-            allItems[i].style.display = "none";
-        }
-        if (result === 0) {
-            rien.style.display = "block";
-        }
-    }
-}
-
 function notif(nom, action) {
 
     const notifContent = document.createElement('li');
